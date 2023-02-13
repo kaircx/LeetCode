2948af0d4c2170664cd5e03f0cc1e1073ecc5741
kaircx <50135106+kaircx@users.noreply.github.com>
2023-02-13 22:34:56 +0900

    Time: 46 ms (74.07%), Space: 14.1 MB (24.58%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def base(number):
+            r=[]
+            tmp=0
+            for a in str(number):
+                if tmp!=a:
+                    r.append(str(1))
+                    r.append(str(a))
+                if tmp == a:
+                    r[len(r)-2]=str(int(r[len(r)-2])+1)    
+                tmp=a
+            return "".join(r)
+        
+        ans=1
+        for i in range(n-1):
+            ans=base(ans)
+            print(ans)
+        return str(ans)
+            
+        
