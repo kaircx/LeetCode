011191026a8141b034a9a69d8ca2647dca6fb891
Inagaki Kai <fllarduino6@gmail.com>
2023-04-07 02:14:01 +0900

    Time: 38 ms (13.45%), Space: 13.9 MB (44.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         tmp =[1,2]
-        a=3
         for i in range(n):
             a=tmp[i-1+2]+tmp[i-2+2]
             tmp.append(a)
