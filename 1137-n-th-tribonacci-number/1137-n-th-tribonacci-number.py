ee27201eae6a068a7d2146a371d70e4c5bafcc5a
Inagaki Kai <fllarduino6@gmail.com>
2023-04-14 02:51:16 +0900

    Time: 34 ms (41.80%), Space: 13.9 MB (6.62%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -3,8 +3,7 @@ class Solution:
         tmp =[0,1,1]
         if n >=3:
             for i in range(n-2):
-                tmp.append(tmp[i]+tmp[i+1]+tmp[i+2])     
-        print(tmp)
+                tmp.append(tmp[i]+tmp[i+1]+tmp[i+2]) 
         return tmp[n]
             
         
