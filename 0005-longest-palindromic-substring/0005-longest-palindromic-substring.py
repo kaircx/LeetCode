271afdcc6f6cbe4a1bd81d18d2509c092f54b408
Inagaki Kai <kaircx@gmail.com>
2023-05-14 20:41:37 +0900

    Time: 4413 ms (22.45%), Space: 16.4 MB (32.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,23 +7,17 @@ class Solution:
             for i in range(len(s)):
                 if s[i]!=s[len(s)-1-i]:
                     return False
-            # print("it is")
             return True
         for a in range(len(s)):
             tmp=s[a]
             index=len(s)
             while tmp in s[a+1:index]:
                 index=s.rfind(tmp,a+1,index)
-                # print("-",tmp,a,index,"-")
-                # print(s[a:index+1])
-                # print("------")
                 if len(ans)<len(s[a:index+1]):
                     if isPalindromic(s[a:index+1]):
                         ans=s[a:index+1]
                 else:
                     break
-                        # print("answer is ",ans)
-                # index-=1
         return ans
         
                     
