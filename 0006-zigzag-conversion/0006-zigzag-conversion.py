8af9fa9ca4716ddfc681efcb6e2f7a245abf2db7
Inagaki Kai <kaircx@gmail.com>
2024-01-10 19:30:34 +0900

    Time: 65 ms (39.95%), Space: 17.8 MB (9.12%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,31 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        ans=[[]for _ in range(numRows)]
+        paper=[]
+        t=numRows
+        flag=False
+        if numRows==1:
+            return s
+        for i in range(len(s)):
+            paper.append(t)
+            if t!=1 and flag==False:
+                t-=1
+            elif t<numRows:
+                t+=1
+                flag=True
+            elif t==numRows:
+                flag=False
+                t-=1
+            else:
+                return s
+        for p,s_x in zip(paper,s):
+            ans[numRows-p].append(s_x)
+        ans= [item for sublist in ans for item in sublist]
+        return ''.join(str(item) for item in ans)
+                
+                
+                
+                
+        
+            
+        
